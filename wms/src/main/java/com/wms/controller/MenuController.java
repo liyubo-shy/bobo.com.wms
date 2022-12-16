982fe662d7c09319b9d4cd24ed4863dae1b8f5ec
liyubo-shy <859591603@qq.com>
2022-12-16 20:16:22 +0800

    2022-12-16 增加自动填充创建时间和更新时间

--- wms/src/main/java/com/wms/controller/MenuController.java
+++ wms/src/main/java/com/wms/controller/MenuController.java
@@ -3,7 +3,6 @@ package com.wms.controller;
 
 import com.wms.common.Result;
 import com.wms.entity.Menu;
-import com.wms.entity.User;
 import com.wms.service.impl.MenuServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.jnlp.ClipboardService;
 import java.util.List;
 
 /**
